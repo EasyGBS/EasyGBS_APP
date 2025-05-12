fa7490396b5dbd1b26c6c07794f3f24c6112a4bf
evin <89688073+bfzg@users.noreply.github.com>
2025-05-12 15:15:41 +0800

    新增显示密码

--- components/ui/input_focuse.vue
+++ components/ui/input_focuse.vue
@@ -1,18 +1,28 @@
 <template>
-	<input
-		:class="isFocused ? 'focused' : 'noraml'"
-		:placeholder="placeholder"
-		placeholder-style="color: #9199a4; "
-		:type="type"
-		:value="modelValue"
-		@input="onInput"
-		@focus="isFocused = true"
-		@blur="isFocused = false"
-	/>
+	<view class="input">
+		<input
+			:class="isFocused ? 'focused' : 'noraml'"
+			:placeholder="placeholder"
+			placeholder-style="color: #9199a4;"
+			:type="realType"
+			:value="modelValue"
+			@input="onInput"
+			@focus="isFocused = true"
+			@blur="isFocused = false"
+		></input>
+		<!-- 仅在 password 类型时显示图标 -->
+		<uni-icons
+			v-if="type === 'password'"
+			:type="showPassword ? 'eye' : 'eye-slash'"
+			size="24"
+			class="eye-icon"
+			@click="toggleShowPassword"
+		/>
+	</view>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const props = defineProps({
 	placeholder: String,
@@ -26,6 +36,18 @@ const props = defineProps({
 const emit = defineEmits(['update:modelValue']); // 派发事件
 
 const isFocused = ref(false);
+const showPassword = ref(false);
+
+const realType = computed(() => {
+	if (props.type === 'password') {
+		return showPassword.value ? 'text' : 'password';
+	}
+	return props.type;
+});
+
+function toggleShowPassword() {
+	showPassword.value = !showPassword.value;
+}
 
 function onInput(e) {
 	emit('update:modelValue', e.detail.value); // 注意小程序 input 的值在 e.detail.value 中
@@ -35,7 +57,7 @@ function onInput(e) {
 <style scoped>
 .input {
 	width: 100%;
-	box-sizing: border-box;
+	position: relative;
 }
 
 .noraml {
@@ -47,8 +69,16 @@ function onInput(e) {
 
 .focused {
 	background-color: #f1f4f9;
-	border: 6rpx solid #1aac19;
+	border: 6rpx solid #1c98f8;
 	border-radius: 18rpx;
 	padding: 28rpx 32rpx;
+}
+
+.eye-icon {
+	position: absolute;
+	right: 42rpx;
+	top: 34rpx;
+	color: #666;
+	z-index: 10;
 }
 </style>
